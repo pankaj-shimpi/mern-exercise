69d27a284bc2f168dd8bd3fabd88c8cb09f879e3
Pankaj Subhash Shimpi <pankajshimpi1190@gmail.com>
2019-12-09 17:12:19 +0530

    some fixes

--- static/src/components/card/card.jsx
+++ static/src/components/card/card.jsx
@@ -4,7 +4,7 @@ export default (props) => {
   return (
     <div className="card-deck col-sm-4">
       <div className="card">
-        <img src={props.user.imgSrc} className="card-img-top" alt="..." />
+        <img src="http://placehold.it/300x200" className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.user.title}</h5>
           <p className="card-text">{props.user.details}</p>
